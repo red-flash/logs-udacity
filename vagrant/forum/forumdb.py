bdd41ef30eb82e648e86d0cd4816902ecf023d45
Andrew Ferguson <andrewferguson94@gmail.com>
2019-06-04 14:43:27 -0400

    Added sql statements for popular auth/article

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,34 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
+import bleach
+
+DBNAME = "forum"
+
+
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect(database=DBNAME)
+  cursor = db.cursor()
+  cursor.execute("select content,time from posts order by time desc;")
+  results = cursor.fetchall()
+  db.close()
+  return results
+  
+  
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  db = psycopg2.connect(database=DBNAME)
+  cursor = db.cursor()
+  #how does this C syntax insert the variable into the insert statement?
+  cursor.execute("insert into posts values (%s)",(bleach.clean(content),))
+  cursor.execute("update posts set content = 'begone!!' where content like '%script%'")
+  cursor.execute("delete from posts where content='begone!!'")
+  db.commit()
+  db.close()
+  
 
 
